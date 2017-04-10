2a2ffb7a3853206b54b783bd007d9ae217f098a7
Jia Ming <jiaming@picoded.com>
2017-04-10 20:52:34 +0800

    added one line method for ease of use

--- utils/automateGetRequests.py
+++ utils/automateGetRequests.py
@@ -1,11 +1,11 @@
-from scrapy.http import FormRequest
 import json
-import urllib
 import requests
 
 class Human():
     strlist = []
     jsonObj = {}
+    lookfor = ""
+    currentExploitCode = ""
     def retrieveFile(self, file):
         self.strlist = []
         with open(file) as fp:
@@ -16,36 +16,43 @@ class Human():
         return self.strlist
 
     def setObjToTest(self, jsonObj):
-        # print type(jsonObj), jsonObj
         self.jsonObj = jsonObj
+
     def insertCodeIntoObj(self, exploit):
         url = self.jsonObj.keys()
         parameters = self.jsonObj[url[0]][1]
         for value in parameters:
             self.jsonObj[url[0]][1][value] = exploit
+        self.currentExploitCode = exploit
         return self.jsonObj
+
+    def expectedResult(self, lookfor):
+        self.lookfor = lookfor
 
     def buildAndSendGETRequest(self, package):
         url = package.keys()
         data = package[url[0]][1]
         data_str = "&".join("%s=%s" % (k,v) for k,v in data.items())
-        print data_str
         result = requests.get(url[0], verify = False, params = data_str)
-        # method = package[url[0]][0]
-        # result = urllib.urlopen(url[0], data)
-        if "root" in result.content:
-            print result.content
+        if self.lookfor in result.content:
+            return (url[0], self.currentExploitCode)
 
-def main():
+def oneLineSetAndGo(injectionPointsFile, exploitCodesFile, expectedResultToSee):
     pq = Human()
-    jsonObjstr = pq.retrieveFile("test.txt")
-    exploitCode = pq.retrieveFile("exploitCode.txt")
+    jsonObjstr = pq.retrieveFile(injectionPointsFile)
+    exploitCode = pq.retrieveFile(exploitCodesFile)
+    pq.expectedResult(expectedResultToSee)
+    successfulResult = []
     for objstr in jsonObjstr:
         pq.setObjToTest(json.loads(objstr))
         for code in exploitCode:
             package = pq.insertCodeIntoObj(code)
-            print package
-            pq.buildAndSendGETRequest(package)
+            result = pq.buildAndSendGETRequest(package)
+            if result is not None:
+                successfulResult.append(result)
+    print successfulResult
 
+def main():
+    oneLineSetAndGo("test.txt", "exploitCode.txt", "root")
 if __name__ == '__main__':
     main()
